fb1e9d5d89a54a9dc1713f34519668aca20c4578
qingfu wang <qingfuw@microsoft.com>
2015-04-27 09:25:22 +0800

    version 1

--- 101-simple-vm-from-image/launch_script.sh
+++ 101-simple-vm-from-image/launch_script.sh
@@ -1,3 +1,8 @@
+for f in install_bosh_client.sh micro_bosh.yml create_cert.sh setup_devbox.py update_os.sh deploy_micro_bosh.sh
+do
+   wget $1/$f
+done
+
 \cp * ../../
 cd ../../
 python setup_devbox.py
