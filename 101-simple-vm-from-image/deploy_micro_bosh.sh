dbb831ae125374a44bb82e81b68cafabaf4b879c
qingfu wang <qingfuw@microsoft.com>
2015-04-27 15:05:00 +0800

    integration test one

--- 101-simple-vm-from-image/deploy_micro_bosh.sh
+++ 101-simple-vm-from-image/deploy_micro_bosh.sh
@@ -5,4 +5,4 @@ rm ~/.bosh_config>/dev/null 2>&1
 rm ~/.bosh_deployer_config>/dev/null 2>&1
 rm bosh-deployments.yml>/dev/null 2>&1
 bosh micro deployment micro_bosh.yml
-bosh micro deploy $1
+bosh micro deploy #stemcell#
--- 101-simple-vm-from-image/setup_devbox.py
+++ 101-simple-vm-from-image/setup_devbox.py
@@ -27,15 +27,15 @@ for f in ['micro_bosh.yml','update_os.sh','deploy_micro_bosh.sh','install_bosh_c
         tmpfile.write(content)
 
 with open (os.path.join('bosh','settings'),"w") as tmpfile:
-    tmpfile.write(str(settings))
+    tmpfile.write(json.dumps(settings, indent=4, sort_keys=True))
 
 call("sh create_cert.sh >> ./bosh/micro_bosh.yml",shell=True)
 call("chmod 700 myPrivateKey.key",shell=True)
 call("cp myPrivateKey.key ./bosh/.ssh/bosh.key",shell=True)
 call("cp -r ./bosh /home/"+settings['username'],shell=True)
 call("chown -R "+settings['username']+" "+"/home/"+settings['username'],shell=True)
-
+call("sh bosh/install_bosh_client.sh",shell=True)
 call("/usr/local/bin/azure config mode asm",shell=True)
 call("/usr/local/bin/azure storage container create --container stemcell -a "+settings['storageaccount']+"-k "+settings['storagekey'],shell=True)
 call("/usr/local/bin/azure storage blob copy start  --dest-account-name "+settings['storageaccount']+"  --dest-container stemcell --dest-blob stemcell.vhd --source-uri '"+settings['stemcelluri']+"' --dest-account-key '"+settings['storagekey']+"' --quiet",shell=True)
-call("bosh/install_bosh_client.sh")
+
